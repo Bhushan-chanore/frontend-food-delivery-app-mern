7fa1c996f24812c61401f6d97ae55006e7eebba2
Bhushan Chanore <107235328+Bhushan-chanore@users.noreply.github.com>
2023-10-12 14:24:48 +0530

    Update MyOrder.js

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -8,7 +8,7 @@ export default function MyOrder() {
 
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'))
-        await fetch("https://backend-food-delivery-app-mern.vercel.app/api/myOrderData", {
+        await fetch("https://bhushan-ir2j.onrender.com/api/myOrderData", {
             // credentials: 'include',
             // Origin:"http://localhost:3000/login",
             method: 'POST',
